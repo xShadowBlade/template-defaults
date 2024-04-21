ad6f65b5de1a4393570673eab88ad62ded20dd28
xShadowBlade <dnguy3n2010@gmail.com>
2024-04-21 15:08:38 -0400

    Add eslint option, install deps, bug fixes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import process, { argv } from "node:process";
 import path from "node:path";
 import fs from "fs-extra";
 import promptGen from "prompt-sync";
+import { exec, execSync } from "node:child_process";
 
 const prompt = promptGen();
 
@@ -39,7 +40,7 @@ if (flags["d"] || flags["debug"]) console.log("Argv", argv, "Args:", args, "\n",
 /**
  * Represents the main entry point for the CLI tool.
  */
-let [, , projectType, projectDir, projectName, projectGitRepo /* copyEslint */] = args;
+let [, , projectType, projectDir, projectName, projectGitRepo, configureEslint, installDep] = args;
 
 /**
  * Cancels the process with a message and code.
@@ -53,7 +54,13 @@ function cancel (message: string = "Cancelled.", code: number = 0) {
 
 // TODO: Add help message.
 if (flags["h"] || flags["help"]) {
-    console.log("Help is coming soon. For now, see https://github.com/xShadowBlade/template-defaults");
+    // console.log("Help is coming soon. For now, see https://github.com/xShadowBlade/template-defaults");
+    console.log(`Usage: template-defaults [projectType] [projectDir] [projectName] [projectGitRepo] [configureEslint] [installDep]
+
+    Options:
+        -d, --debug: Debug mode.
+        -y, --yes:  Skip prompts.
+        -h, --help: Show help message.`);
     cancel("", 0);
 }
 
@@ -64,6 +71,34 @@ if (!(flags["y"] || flags["yes"])) {
     projectDir = projectDir ?? ".";
     projectName = projectName ?? "my-project";
     projectGitRepo = projectGitRepo ?? "https://github.com/xShadowBlade/template-defaults";
+    configureEslint = configureEslint ?? "y";
+    installDep = installDep ?? "y";
+}
+interface PromptOptions {
+    initPrompt: string;
+    defaultMessage?: string;
+    defaultValue: string;
+    cancelMessage?: string;
+    cancelCode?: number;
+}
+
+/**
+ * Function to set the default prompt for a value.
+ * @param options - The options for the prompt.
+ * @returns The prompted value.
+ */
+function setDefaultPrompt (options: PromptOptions) {
+    const { initPrompt, defaultMessage, defaultValue, cancelMessage, cancelCode } = options;
+
+    let prompted = prompt(initPrompt);
+    if (prompted === null) cancel(cancelMessage, cancelCode);
+    prompted = prompted.toLowerCase().trim().replace(/[^a-z-]/g, "");
+    if (!prompted) {
+        console.log(defaultMessage ?? `Using default value '${defaultValue}'.`);
+        prompted = defaultValue;
+    }
+    console.log("");
+    return prompted;
 }
 
 /**
@@ -73,33 +108,6 @@ function walkThrough () {
     console.log("This CLI tool will create a new project in the specified directory.");
     console.log("For more information, see https://github.com/xShadowBlade/template-defaults");
     console.log("Press ^C at any time to cancel. \n");
-
-    interface PromptOptions {
-        initPrompt: string;
-        defaultMessage?: string;
-        defaultValue: string;
-        cancelMessage?: string;
-        cancelCode?: number;
-    }
-
-    /**
-     * Function to set the default prompt for a value.
-     * @param options - The options for the prompt.
-     * @returns The prompted value.
-     */
-    function setDefaultPrompt (options: PromptOptions) {
-        const { initPrompt, defaultMessage, defaultValue, cancelMessage, cancelCode } = options;
-
-        let prompted = prompt(initPrompt);
-        if (prompted === null) cancel(cancelMessage, cancelCode);
-        prompted = prompted.toLowerCase().trim().replace(/[^a-z-]/g, "");
-        if (!prompted) {
-            console.log(defaultMessage ?? `Using default value '${defaultValue}'.`);
-            prompted = defaultValue;
-        }
-
-        return prompted;
-    }
 
     projectType = projectType ? (() => {
         console.log("Project type: ", projectType);
@@ -145,6 +153,28 @@ function walkThrough () {
         // cancelCode: 1
     });
 
+    configureEslint = configureEslint ? (() => {
+        console.log("Configure ESLint: ", configureEslint);
+        return configureEslint;
+    })() : setDefaultPrompt({
+        initPrompt: "Configure ESLint (y/n) [y]: ",
+        defaultMessage: "Using default value 'y'.",
+        defaultValue: "y",
+        // cancelMessage: "No ESLint configuration specified. Exiting.",
+        // cancelCode: 1
+    });
+
+    installDep = installDep ? (() => {
+        console.log("Install dependencies: ", installDep);
+        return installDep;
+    })() : setDefaultPrompt({
+        initPrompt: "Install dependencies (y/n) [y]: ",
+        defaultMessage: "Using default value 'y'.",
+        defaultValue: "y",
+        // cancelMessage: "No dependencies installation specified. Exiting.",
+        // cancelCode: 1
+    });
+
     // Ask for confirmation.
     const confirmation = (prompt("Confirm (y/n) [y]: ") ?? "").toLowerCase();
     if (!["", "y", "yes"].includes(confirmation)) {
@@ -155,23 +185,24 @@ function walkThrough () {
 const projectDirPath = path.join(process.cwd(), projectDir);
 // console.log("Project dir: ", projectDirPath);
 
+const distDir = path.join(__filename, "..");
+// console.log("Dist dir: ", distDir);
+
 // Copy all files from ../common/** to the project directory.
-const commonDir = path.join(__dirname, ".", "common");
+const commonDir = path.join(distDir, "common");
 // console.log("Common dir: ", commonDir);
 fs.copySync(commonDir, projectDirPath);
 
 const projectTypeTemplateDir = (() => {
     switch (projectType) {
-    case "react-ts": return path.join(__dirname, ".", "templates", "react-ts");
-    case "html-ts": return path.join(__dirname, ".", "templates", "html-ts");
-    case "ts": default: return path.join(__dirname, ".", "templates", "ts");
+    case "react-ts": return path.join(distDir, "templates", "react-ts");
+    case "html-ts": return path.join(distDir, "templates", "html-ts");
+    case "ts": default: return path.join(distDir, "templates", "ts");
     }
 })();
 
 // Copy all files from the template dir to the project directory.
 if (projectTypeTemplateDir) {
-    // console.log("Project type: ", projectType);
-    // console.log("Template dir: ", projectTypeTemplateDir);
     fs.copySync(projectTypeTemplateDir, projectDirPath);
 }
 
@@ -196,14 +227,68 @@ const packageJsonPath = path.join(projectDirPath, "package.json");
 // packageJson.bugs.url = `${projectGitRepo}/issues`;
 // packageJson.homepage = `${projectGitRepo}#readme`;
 
+if (!configureEslint || !["y", "yes", "true"].includes(configureEslint)) {
+    fs.removeSync(path.join(projectDirPath, ".eslintrc.json"));
+    const eslintDevDeps = [
+        "eslint",
+        "eslint-config-prettier",
+        "eslint-plugin-jsdoc",
+        "@typescript-eslint/eslint-plugin",
+        "@typescript-eslint/parser",
+        "prettier",
+        "prettier-eslint",
+        "eslint-config-react-app",
+        "eslint-plugin-react",
+    ];
+    // replaceInFile(packageJsonPath, /\n {4}"eslint": "\^8.52.0",\n {4}"eslint-config-prettier": "\^9.0.0",\n {4}"eslint-plugin-jsdoc": "\^46.9.0",\n {4}/, "");
+    eslintDevDeps.forEach((dep) => {
+        replaceInFile(packageJsonPath, new RegExp(`\n {4}"${dep}": ".*",`), "");
+    });
+    // Remove empty lines.
+    replaceInFile(packageJsonPath, /^\s*$/gm, "");
+}
+
 replaceInFile(packageJsonPath, /"name": ".*"/, `"name": "${projectName}"`);
 replaceInFile(packageJsonPath, /"url": ".*"/, `"url": "${projectGitRepo}"`);
 replaceInFile(packageJsonPath, /"url": ".*\/issues"/, `"url": "${projectGitRepo}/issues"`);
 replaceInFile(packageJsonPath, /"homepage": ".*"/, `"homepage": "${projectGitRepo}#readme"`);
 
-console.log("Done!");
-console.log("To get started, run the following commands:");
-console.log("npm install");
-if (["react-ts", "html-ts"].includes(projectType)) console.log("npm start");
+installDep = installDep ? (() => {
+    console.log("Install dependencies: ", installDep);
+    return installDep;
+})() : setDefaultPrompt({
+    initPrompt: "Install dependencies (y/n) [y]: ",
+    defaultMessage: "Using default value 'y'.",
+    defaultValue: "y",
+    // cancelMessage: "No dependencies installation specified. Exiting.",
+    // cancelCode: 1
+});
+
+if (installDep && ["y", "yes", "true"].includes(installDep)) {
+    console.log("\nInstalling dependencies...");
+
+    try {
+        execSync("npm install", { cwd: projectDirPath, stdio: "inherit" });
+    } catch (error) {
+        console.error("Failed to install dependencies.", error);
+        process.exit(1);
+    }
+}
+
+console.log("\nDone!");
+const messages = [
+    {
+        message: "npm install",
+        condition: installDep && ["y", "yes", "true"].includes(installDep),
+    },
+    {
+        message: "npm start",
+        condition: ["react-ts", "html-ts"].includes(projectType),
+    },
+];
+if (messages.some((msg) => msg.condition)) console.log("To get started, run the following commands:");
+messages.forEach((msg) => {
+    if (msg.condition) console.log(msg.message);
+});
 
 process.exit(0);
