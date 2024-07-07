1433dedb2c135cec3fd37a47c26ed4f036c5b96b
zBlade <dnguy3n2010@gmail.com>
2024-07-07 17:42:46 -0400

    Update eslint and webpack config

--- dist/templates/html-ts/webpack.config.js
+++ dist/templates/html-ts/webpack.config.js
@@ -4,10 +4,10 @@
 /* eslint-disable jsdoc/check-tag-names */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const { EsbuildPlugin } = require("esbuild-loader");
 const webpack = require("webpack");
 // in case you run into any typescript error when configuring `devServer`
 require("webpack-dev-server");
+const { EsbuildPlugin } = require("esbuild-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
@@ -15,13 +15,12 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = (env, argv) => {
-    console.log("Args:", argv);
     const mode = argv.mode;
     /**
      * @type {import("webpack").Configuration}
      */
     const options = {
-        entry: "./src/index.ts", // Entry point of your application
+        entry: "./src/index.tsx", // Entry point of your application
         output: {
             path: path.resolve(__dirname, "build"),
             filename: "bundle.js", // Output bundle file name
@@ -29,16 +28,12 @@ module.exports = (env, argv) => {
         resolve: {
             extensions: [".css", ".tsx", ".ts", ".js", "..."],
             alias: {
-                // "@": path.resolve(__dirname, "src"),
                 // Uncomment the following if you use emath.js
                 // "emath.js": "emath.js/ts",
                 // "emath.js/game": "emath.js/ts/game",
+                // "emath.js/presets": "emath.js/ts/presets",
             },
         },
-        // watch: true,
-        // watchOptions: {
-        //     ignored: "**/node_modules",
-        // },
         module: {
             rules: [
             // Use esbuild to compile JavaScript & TypeScript
@@ -60,8 +55,6 @@ module.exports = (env, argv) => {
                     use: [
                         mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
                         "css-loader",
-                        // "postcss-loader",
-                        // "sass-loader",
                     ],
                 },
             ],
@@ -69,7 +62,7 @@ module.exports = (env, argv) => {
         optimization: {
             minimizer: [
                 new EsbuildPlugin({
-                    target: "es2015", // Syntax to transpile to (see options below for possible values)
+                    target: "es2015", // Syntax to transpile to
                 }),
                 new CssMinimizerPlugin(),
             ],
@@ -78,13 +71,9 @@ module.exports = (env, argv) => {
             new HtmlWebpackPlugin({
                 template: "./public/index.html", // Use this HTML file as a template
             }),
-            // new webpack.DefinePlugin({
-            //     // "%PUBLIC_URL%": JSON.stringify(mode === "production" ? "../public/" : "./"),
-            //     MODE: JSON.stringify(mode),
-            // }),
+            // Define global constants
             new EsbuildPlugin({
                 define: {
-                    // "%PUBLIC_URL%": JSON.stringify(mode === "production" ? "../public/" : "./"),
                     MODE: JSON.stringify(mode),
                 },
             }),
@@ -99,9 +88,6 @@ module.exports = (env, argv) => {
     };
     if (mode === "production") {
         options.plugins.push(
-            // new webpack.optimize.LimitChunkCountPlugin({
-            //     maxChunks: 1,
-            // }),
             // Copies public folder to dist folder
             new CopyPlugin({
                 patterns: [
@@ -118,12 +104,6 @@ module.exports = (env, argv) => {
         );
     } else if (mode === "development") {
         options.devtool = "eval-cheap-module-source-map";
-        // options.devServer = {
-        //     contentBase: path.join(__dirname, "build"),
-        //     compress: true,
-        //     port: 3000,
-        //     historyApiFallback: true,
-        // };
     }
     return options;
 };
--- dist/templates/react-ts/src/index.tsx
+++ dist/templates/react-ts/src/index.tsx
@@ -5,5 +5,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root") as HTMLElement);
+const root = createRoot(document.getElementById("root") ?? document.body);
 root.render(<div>Hello World!</div>);
